3170a3a108bcd3f55b7a81d46154374e4caba8d7
daquintero <dario@flexcompute.com>
2024-01-05 17:43:03 +0100

    FIX: Try to get tests to pass again

--- tidy3d/components/types.py
+++ tidy3d/components/types.py
@@ -200,8 +200,10 @@ ClipOperationType = Literal["union", "intersection", "difference", "symmetric_di
 BoxSurface = Literal["x-", "x+", "y-", "y+", "z-", "z+"]
 
 # TODO Complicated as trimesh should be a core package unless decoupled implementation types in functional location.
+#  We need to restructure.
 if check_import("trimesh"):
-    import trimesh # Won't add much overhead if already imported
+    import trimesh  # Won't add much overhead if already imported
+
     TrimeshType = trimesh.Trimesh
 else:
     TrimeshType = Any
